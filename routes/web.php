5e2623e20e894cf263218863ebb1bca2c087af84
roi <1115338663@qq.com>
2020-12-20 07:32:47 +0000

    基础页面

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/',"StaticPageControlle@home");
+Route::get('/',"StaticPageController@home");
 Route::get('/help',"StaticPageController@help");
 Route::get('/about',"StaticPageController@about");
