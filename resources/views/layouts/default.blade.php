5ffe01661e90f7521fbcce4a13704bf90c8e94e4
roi <1115338663@qq.com>
2020-12-20 08:02:35 +0000

    初始化样式

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -2,8 +2,24 @@
 <html>
     <head>
         <title>@yield('title', 'WeiBo App')</title>
+        <link rel="stylesheet" href="/css/app.css">
     </head>
     <body>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container">
+            <a href="/" class="navbar-brand">WeiBo App</a>
+            <ul class="navbar-nav justify-content-end">
+                <li class="nav-item">
+                    <a class="nav-link" href="/help">帮助</a>
+                </li>
+                <li class="navbar-nav justify-content-end">
+                    <a class="nav-link" href="#">登录</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+    <div class="container">
         @yield('content')
+    </div>
     </body>
 </html>
--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -1,5 +1,13 @@
 @extends('layouts.default')
 @section('title', '主页')
 @section('content')
-    <h1>主页</h1>
+    <div class="jumbotron">
+        <h1>Hello Laravel</h1>
+        <p class="lead">
+            <a href="/help">需要帮助？</a>
+        </p>
+        <p>
+            <a  class="btn btn-success btn-lg">登录</a>
+        </p>
+    </div>
 @stop
