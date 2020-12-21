6baeff0c9200a005cae618707e6d7053fa623e66
roi <1115338663@qq.com>
2020-12-21 09:13:41 +0000

    增加CSRF验证

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -15,7 +15,12 @@ class UsersController extends Controller
         return view('users.show', compact('user'));
     }
 
-    public function store() {
-        return view('users.store');
+    public function store(Request $request) {
+        $this->validate($request, [
+           'name' => 'required|unique:users|max:50',
+            'email' => 'required|email|unique:users|max:255',
+            'password' => 'required|confirmed|min:6'
+        ]);
+        return;
     }
 }
