b3c11a49fee7bab1ca9f8c4c7b6aa6f1c63bcc63
roi <1115338663@qq.com>
2020-12-21 08:57:23 +0000

    显示用户页面

--- app/Models/User.php
+++ app/Models/User.php
@@ -11,6 +11,9 @@ class User extends Authenticatable
 {
     use HasFactory, Notifiable;
 
+    protected $table = "users";
+
+
     /**
      * The attributes that are mass assignable.
      *
@@ -40,4 +43,10 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+
+    public function gavatar($size = 100) {
+        $hash = md5(strtolower(trim($this->attributes['email'])));
+        return "http://www.gravatar.com/avatar/$hash?s=$size";
+    }
 }
