340985d7e09a626d24a8e027bbf9da390bb2f317
roi <1115338663@qq.com>
2020-12-23 08:16:52 +0000

    创建会话

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -7,10 +7,10 @@
             <a href="{{route('help')}}">需要帮助？</a>
         </p>
         <p>
-            <a  class="btn btn-success btn-lg">登录</a>
+            <a  href="{{route('login')}}" class="btn btn-success btn-lg">登录</a>
         </p>
         <p>
-            <a  class="btn btn-success btn-lg" href="{{route('singup')}}">现在注册</a>
+            <a  class="btn btn-success btn-lg" href="{{route('signup')}}">现在注册</a>
         </p>
     </div>
 @stop
