2344c46bc7fb4f7ddeb1bd27d8206928f36d4299
roi <1115338663@qq.com>
2020-12-24 07:57:55 +0000

    添加权限

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -7,6 +7,14 @@ use Illuminate\Support\Facades\Auth;
 
 class SessionsController extends Controller
 {
+
+    public function __construct()
+    {
+        $this->middleware('guest', [
+            'only' => ['create']
+        ]);
+    }
+
     public function create() {
         return view('sessions.create');
     }
@@ -18,7 +26,8 @@ class SessionsController extends Controller
         ]);
         if (Auth::attempt($credentials, $request->has('remember'))) {
             session()->flash('success', '登录成功');
-            return redirect()->route('users.show', [Auth::user()]);
+            $fallback = route('users.show', [Auth::user()]);
+            return redirect()->intended($fallback);
         }else {
             session()->flash('danger', '用户名或密码错误');
             return redirect()->back()->withInput();
--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -8,6 +8,18 @@ use Illuminate\Support\Facades\Auth;
 
 class UsersController extends Controller
 {
+
+    public function __construct()
+    {
+        $this->middleware('auth', [
+           'except' => ['show', 'create', 'store']
+        ]);
+
+        $this->middleware('guest', [
+            'only' => ['create']
+        ]);
+    }
+
     public function create () {
         return view('users.create');
     }
@@ -34,10 +46,12 @@ class UsersController extends Controller
     }
 
     public function edit(User $user) {
+        $this->authorize('update', $user);
         return view('users.edit', compact('user'));
     }
 
     public function update(User $user, Request $request) {
+        $this->authorize('update', $user);
         $credntials = $this->validate($request, [
            'name' => 'required|max:50',
             'password' => 'nullable|confirmed|min:6'
