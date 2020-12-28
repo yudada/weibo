806c839b90ab1669327ed5e3d0e00605aadf6ca6
roi <1115338663@qq.com>
2020-12-28 07:06:38 +0000

    查看用户列表

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -6,11 +6,13 @@
         <p class="lead">
             <a href="{{route('help')}}">需要帮助？</a>
         </p>
+        @if(!Auth::check())
         <p>
             <a  href="{{route('login')}}" class="btn btn-success btn-lg">登录</a>
         </p>
         <p>
             <a  class="btn btn-success btn-lg" href="{{route('signup')}}">现在注册</a>
         </p>
+            @endif
     </div>
 @stop
