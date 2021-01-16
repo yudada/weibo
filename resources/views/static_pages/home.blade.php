9c76e760ceaeea1ac85de608339b02d2dc5d455e
roi <1115338663@qq.com>
2021-01-16 01:51:03 +0000

    发布微博

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -1,18 +1,31 @@
 @extends('layouts.default')
-@section('title', '主页')
+
 @section('content')
-    <div class="jumbotron">
-        <h1>Hello Laravel</h1>
-        <p class="lead">
-            <a href="{{route('help')}}">需要帮助？</a>
-        </p>
-        @if(!Auth::check())
-        <p>
-            <a  href="{{route('login')}}" class="btn btn-success btn-lg">登录</a>
-        </p>
-        <p>
-            <a  class="btn btn-success btn-lg" href="{{route('signup')}}">现在注册</a>
-        </p>
-            @endif
-    </div>
+    @if (Auth::check())
+        <div class="row">
+            <div class="col-md-8">
+                <section class="status_form">
+                    @include('shared._statuses_form')
+                </section>
+            </div>
+            <aside class="col-md-4">
+                <section class="user_info">
+                    @include('shared._user_info', ['user' => Auth::user()])
+                </section>
+            </aside>
+        </div>
+    @else
+        <div class="jumbotron">
+            <h1>Hello Laravel</h1>
+            <p class="lead">
+                你现在所看到的是 <a href="https://learnku.com/courses/laravel-essential-training">Laravel 入门教程</a> 的示例项目主页。
+            </p>
+            <p>
+                一切，将从这里开始。
+            </p>
+            <p>
+                <a class="btn btn-lg btn-success" href="{{ route('signup') }}" role="button">现在注册</a>
+            </p>
+        </div>
+    @endif
 @stop
