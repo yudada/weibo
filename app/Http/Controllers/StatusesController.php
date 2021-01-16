df5bf2c741f91f1036363fcbf889c6ae5aeb5979
roi <1115338663@qq.com>
2021-01-16 02:12:01 +0000

    删除微博

--- app/Http/Controllers/StatusesController.php
+++ app/Http/Controllers/StatusesController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Status;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -22,6 +23,16 @@ class StatusesController extends Controller
         ]);
 
         session()->flash('success', '发布成功');
+        return redirect()->back();
+    }
+
+    public function destroy(Status $status) {
+        $this->authorize('destroy', $status);
+
+        $status->delete();
+
+        session()->flash('success', '删除微博成功');
+
         return redirect()->back();
     }
 }
